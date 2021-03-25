23b116485f60067a222704fe1e9bc5047eaece5a
Ilia <kassirovilia@icloud.com>
2021-03-25 12:12:03 +0300

    Graphic Interface

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -11,13 +11,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ChatWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("ChatWindow");
+        primaryStage.setScene(new Scene(root, 400, 700));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
